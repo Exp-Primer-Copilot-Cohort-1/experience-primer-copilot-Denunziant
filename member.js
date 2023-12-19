d3132389884aad518423bae4188d1cfcfb344b87
Sergej Semenishchev <144904765+Denunziant@users.noreply.github.com>
2023-12-19 20:04:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsmember()) {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Django', 'Flask']
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    }
+    console.log(member.skills[4])
+}
